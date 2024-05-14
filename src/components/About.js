be6911db2bdae7c42d427f3351417569aadc5a13
Lexi Miller <lexim515@gmail.com>
2024-05-14 13:02:41 -0400

    headling

--- src/components/About.js
+++ src/components/About.js
@@ -7,7 +7,7 @@ export default function About() {
         <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
           <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium">
             Hi, I'm Lexi &
-            <br className="hidden lg:inline-block" />I Have a Passion For
+            <br className="hidden lg:inline-block" /> I Have a Passion For
             Problem Solving
           </h1>
           <p className="mb-8 leading-relaxed">
